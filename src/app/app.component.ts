f30d764e8b3a0de3721f5c429ead5500bd71d241
Johan Vargas <jnvarco@gmail.com>
2017-10-01 20:39:06 -0600

    inicio cenfo

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cenfo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Johan';
+}
